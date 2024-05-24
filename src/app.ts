0ac29fcd93faa8a021710ed94d8861758233977e
pedro <pedroalberti2003@gmail.com>
2024-05-24 15:06:43 -0300

    Adición de GET ALL, GET ONE y POST

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,39 @@
-import express from 'express'
+import express from 'express' 
+import { User } from './user.js'
 
 const app = express()
+app.use(express.json())
 
-app.use('/',(req,res) => {
-    res.json({message: '<h1>Hello world!!</h1>'})
+const users = [
+    new User(
+        'paulaperez',
+        'Paula',
+        'Perez',
+        '12345',
+        'paulaperez@gmail.com',
+        'a02b91bc-3769-4221-beb1-d7a3aeba7dad'
+    ),
+]
+
+app.get('/api/users',(req,res) => {
+    res.json(users) 
+})
+
+app.get('/api/users/:id',(req,res) => {
+    const user = users.find((user)=>user.id===req.params.id)
+    if (!user) {
+        res.status(404).send({message: 'User not found'})
+    }
+    res.json(user)
+})
+
+app.post('/api/users',(req,res) =>{
+    const {username, name, surname, password, mail} = req.body
+
+    const user = new User(username ,name, surname, password, mail)
+
+    users.push(user)
+    res.status(201).send({message:'Character created',data:user})
 })
 
 app.listen(3000,() => {
--- src/user.ts
+++ src/user.ts
@@ -2,12 +2,12 @@ import crypto from 'node:crypto'
 
 export class User{
     constructor(
-        public id = crypto.randomUUID(),
         public username: string,
         public name: string,
         public surname: string,
         private password: string,
         public mail: string,
+        public id = crypto.randomUUID()
         //public sessions: session[], el usuario conoce sus sesiones, se crearan mas adelante
         //public tasks: task[], el usurio conoce sus tareas, se crearan mas adelante
     ){}
